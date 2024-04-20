ba6f4354a2ffa59b13a0933849d3f7ce02c72d0b
Anoop Singh <anoop102910@gmail.com>
2024-04-20 15:38:09 +0530

    fixed a small bug in navbar comp

--- src/components/shared/navbar.jsx
+++ src/components/shared/navbar.jsx
@@ -8,15 +8,22 @@ import { useState } from "react";
 
 function Navbar() {
   const { data, status } = useSession();
-  if (status === "loading") return <div>Loading..</div>;
-
   const [hide, toggle] = useState(true);
+
+  if (status === "loading") return <div>Loading..</div>;
 
   return (
     <>
-      <header onClick={()=>toggle(!hide)} className="sticky z-[1000] top-0 left-0 w-full gap-4 h-16 rounded-md shadow-md bg-white flex items-center px-6 justify-between">
-        {hide && <Dashboard className={` lg:hidden absolute top-0 left-0 min-h-screen `}  />}
-        <Icon onClick={()=>toggle(!hide)} icon="mingcute:menu-fill" className="text-3xl lg:hidden text-slate-700"/>
+      <header
+        onClick={() => toggle(!hide)}
+        className="sticky z-[1000] top-0 left-0 w-full gap-4 h-16 rounded-md shadow-md bg-white flex items-center px-6 justify-between"
+      >
+        {hide && <Dashboard className={` lg:hidden absolute top-0 left-0 min-h-screen `} />}
+        <Icon
+          onClick={() => toggle(!hide)}
+          icon="mingcute:menu-fill"
+          className="text-3xl lg:hidden text-slate-700"
+        />
         <div className="sm:w-[35vw] w-full   rounded-md  flex gap-4 px-4 items-center bg-slate-100 py-2">
           <SearchInput />
         </div>
